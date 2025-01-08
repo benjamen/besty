81fc04b77143ae4256bb6e3a91272c2da1ae0607
benwa <benwa@NITRO5>
2025-01-08 03:30:21 +0000

    <your commit message here>

---
+++ besty/besty/doctype/product_category/test_product_category.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2025, Benjamen Walsh and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestProductCategory(FrappeTestCase):
+	pass
